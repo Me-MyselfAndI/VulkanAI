d3ea2194226a722a6850dfd598e1f4d044781c2a
Mikita Slabysh <slabysh2015@gmail.com>
2023-07-26 16:32:46 -0400

    Made UI render and work

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from views import views
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
