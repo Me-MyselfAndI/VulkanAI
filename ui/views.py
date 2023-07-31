5299d4df753b80068438e94a924a87bbb5817c38
Mikita Slabysh <slabysh2015@gmail.com>
2023-07-30 22:19:22 -0400

    render search result

--- ui/views.py
+++ ui/views.py
@@ -1,9 +1,21 @@
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for, render_template_string
+from web_search.search_engine import Search_Engine
+
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route("/search-result")
+def search_result():
+    # Create the engine
+    search_engine = Search_Engine()
+    # Use update-links method to refresh the search results (stored inside the class).
+    # Start entry is 0 by default, it's the pagination offset
+    search_engine.update_links("Chupa-chups", start_entry=0)
+    # Open link (default opens 0th link, otherwise use link_number argument)
+    return render_template_string(search_engine.get_first_result_html())
 
 
