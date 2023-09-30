01dca40e4e4294d0eb65e5ea4de73f846551b923
Mikita Slabysh <slabysh2015@gmail.com>
2023-09-30 16:11:10 -0400

    Working on getting input from frontend

--- ui/static/script.js
+++ ui/static/script.js
@@ -9,10 +9,22 @@ showLoader = function(e) {
 }
 
 document.getElementById("search").addEventListener("submit", function(event) {
-  event.preventDefault();
-  showLoader(event);
-  window.location.href = "http://127.0.0.1:8000/views/search-result";
+    event.preventDefault();
+    //showLoader(event);
+    let inputValue = document.getElementById("search")[0].value;
+    console.log(inputValue);
+    let $;
+    $.ajax = function (param) {
 
+    }
+    //window.location.href = "http://127.0.0.1:8000/views/search-result";
+    $.ajax({
+      type: "POST",
+      url: "..\views.py",
+      data: { param: text}
+    }).done(function() {
+
+    });
 });
 
 
