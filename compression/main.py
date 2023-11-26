fd7363f813fbd287170ccb92b9f6651e68535b05
gpodoksik3 <gpodoksik3@gatech.edu>
2023-11-26 13:33:55 -0500

    Added main scraping function

--- compression/main.py
+++ compression/main.py
@@ -4,10 +4,16 @@ from compression.ai.gpt_engine import GPTEngine
 
 class ScrapingController:
     def __init__(self):
-        self.gpt = GPTEngine('')
+        self.gpt = GPTEngine()
+        # self.astica = AsticaEngine()
 
     def get_parsed_website_html(self, website, search_query):
         # website: {
         #     'url': website,
         #     'html': html
         # }
+        parser = Parser(website['html'])
+        menu_items = parser.find_website_menu(website['url'])
+
+        crawler = Crawler(website['url'], self.gpt)
+        return crawler.navigate_to_relevant_page(search_query, menu_items)
